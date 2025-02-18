2c572a65c78dc4b5ce1f8bdee7153bd5511d71a9
Sam <sjwilliams388@gmail.com>
2025-02-18 09:48:03 +0000

    passing messages to the console

--- client/script.js
+++ client/script.js
@@ -1,1 +1,11 @@
-const ws = new WebSocket("wss://localhost:8082");
+const ws = new WebSocket("ws://localhost:8082");
+
+ws.addEventListener("open", () => {
+    console.log("We are connected!");
+
+    ws.send("Hey, how's it going?");
+});
+
+ws.addEventListener("message", e => {
+    console.log(e);
+});
