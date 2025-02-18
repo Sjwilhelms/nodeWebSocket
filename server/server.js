c6098b8e5b51ea020cd4009cbd05b3dd214ea3ad
Sam <sjwilliams388@gmail.com>
2025-02-18 18:43:24 +0000

    add functionality for simulataneous clients

--- server/server.js
+++ server/server.js
@@ -2,16 +2,32 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8082 });
 
+const clients = new Set();
+
 wss.on("connection", ws => {
     console.log("New client connected!");
+    clients.add(ws);
 
     ws.on("message", data => {
-        console.log(`Client has sent us: ${data}`);
-        const message = data.toString();
-        ws.send(message.toUpperCase());
+        const messageString = data.toString();
+        console.log(`Client has sent us: ${messageString}`);
+
+
+        clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(messageString);
+            }
+        });
     });
 
     ws.on("close", () => {
         console.log("The client has disconnected.");
+        clients.delete(ws);
+    });
+
+    // error handling
+    ws.on("error", (error) => {
+        console.error("WebSocket error:", error);
+        clients.delete(ws);
     });
 });
