bf0ed1d02d7554da6f1faa084e7b8004616e49cc
Sam <sjwilliams388@gmail.com>
2025-02-18 09:47:36 +0000

    passing messages to the console

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
-const { WebSocketServer } = require("ws");
 const WebSocket = require("ws");
 
-const ws = new WebSocket.Server({ port: 8082 });
+const wss = new WebSocket.Server({ port: 8082 });
 
-wss.on("Connection", ws => {
+wss.on("connection", ws => {
     console.log("New client connected!");
+
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`);
+        const message = data.toString();
+        ws.send(message.toUpperCase());
+    });
+
+    ws.on("close", () => {
+        console.log("The client has disconnected.");
+    });
 });
