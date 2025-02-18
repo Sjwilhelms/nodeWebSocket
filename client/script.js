4778eb33a2c0590008a1830b83dc48806424a75d
Sam <sjwilliams388@gmail.com>
2025-02-18 13:07:56 +0000

    add some dom manipulation

--- client/script.js
+++ client/script.js
@@ -2,15 +2,22 @@ const ws = new WebSocket("ws://localhost:8082");
 let inputMessage = document.getElementById("input-message");
 let sendButton = document.getElementById("submit");
 
+
 function sendMessage() {
     console.log(inputMessage.value);
+    const chatHistory = document.getElementById("chat-history");
+    let outputMessage = document.createElement("div");
+    chatHistory.appendChild(outputMessage);
+    outputMessage.innerHTML = `<div class= "message-bubble sent-messages">${inputMessage.value}<div class="timestamp">${Date.now()}</div></div>`;
+
     inputMessage.value = "";
 }
 
 ws.addEventListener("open", () => {
     console.log("We are connected!");
 
-    ws.send("Hey, how's it going?");
+
+    ws.send(inputMessage.value);
 });
 
 ws.addEventListener("message", e => {
